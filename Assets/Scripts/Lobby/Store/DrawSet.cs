df040a9111d779ce1d58fa00710ed8cf8ad0e095
ehdgjs100 <ehdgjs1000@naver.com>
2025-06-23 22:10:40 +0900

    Store
    Draw Card

--- Assets/Scripts/Lobby/Store/DrawSet.cs
+++ Assets/Scripts/Lobby/Store/DrawSet.cs
@@ -10,10 +10,21 @@ public class DrawSet : MonoBehaviour
     [SerializeField] GameObject mainCamera;
     [SerializeField] GameObject drawPanel;
 
+    [SerializeField] GameObject[] normalHeros;
+    [SerializeField] GameObject[] specialHeros;
+    [SerializeField] GameObject[] epicHeros;
+    [SerializeField] GameObject[] legendHeros;
+
+    [SerializeField] DrawHeroGo draw1Hero;
+    [SerializeField] DrawHeroGo[] draw10Heros;
+
+    float cardMoveTime = 0.5f;
+    bool canExit = false;
+    [SerializeField] GameObject exitText;
 
     public void DrawCardOnClick(int drawNum)
     {
-        if (drawNum == 1) StartCoroutine(DrawHeros(drawNum));
+        StartCoroutine(DrawHeros(drawNum));
 
     }
     IEnumerator DrawHeros(int drawNum)
@@ -26,16 +37,82 @@ public class DrawSet : MonoBehaviour
         yield return new WaitForSeconds(1.2f);
         drawPanel.SetActive(true);
         mainCamera.transform.position = initCameraPos;
-
-        if(drawNum == 1)
+        if (drawNum == 1)
         {
-
+            draw1Hero.SetHero(RandomReturnHero());
+            draw1Hero.InfoUpdate();
+            draw1Hero.gameObject.transform.DOScale(Vector3.one, 1.0f);
         }
         else
         {
-
+            for (int i = 0; i < 10; i++)
+            {
+                draw10Heros[i].SetHero(RandomReturnHero());
+                draw10Heros[i].InfoUpdate();
+                draw10Heros[i].gameObject.transform.DOScale(new Vector3(0.4f,0.4f,0.4f), 0);
+                if(i == 0 ) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(0,710,0), cardMoveTime);
+                else if(i == 1) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(350, 600, 0), cardMoveTime);
+                else if (i == 2) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(350, 200, 0), cardMoveTime);
+                else if (i == 3) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(350, -200, 0), cardMoveTime);
+                else if (i == 4) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(350, -600, 0), cardMoveTime);
+                else if (i == 5) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(0, -710, 0), cardMoveTime);
+                else if (i == 6) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(-350, -600, 0), cardMoveTime);
+                else if (i == 7) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(-350, -200, 0), cardMoveTime);
+                else if (i == 8) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(-350, 200, 0), cardMoveTime);
+                else if (i == 9) draw10Heros[i].gameObject.transform.DOLocalMove(new Vector3(-350, 600, 0), cardMoveTime);
+                yield return new WaitForSeconds(0.6f);
+            }
+        }
+        yield return new WaitForSeconds(1.0f);
+        exitText.SetActive(true);
+        canExit = true;
+    }
+    public void ExitDrawHeroCard()
+    {
+        if (canExit)
+        {
+            draw1Hero.gameObject.transform.localPosition = new Vector3(0, 0, 0);
+            draw1Hero.gameObject.transform.DOScale(Vector3.zero, 0);
+            for (int i = 0; i < 10; i++)
+            {
+                draw10Heros[i].gameObject.transform.localPosition = new Vector3(0, 0, 0);
+                draw10Heros[i].gameObject.transform.DOScale(Vector3.zero, 0);
+            }
+            canExit = false;
+            exitText.SetActive(false);
+            drawPanel.SetActive(false);
         }
 
+    }
+
+    /// <summary>
+    /// »Ì±âÈ®·ü 0 = 70% / 1 = 25% / 2 = 4% / 3 = 1%
+    /// </summary>
+    /// <returns></returns>
+    private GameObject RandomReturnHero()
+    {
+        GameObject drawHeroGo = null ;
+        float randomProb = Random.Range(0.0f, 100.0f);
+        if(randomProb <= 70.0f)
+        {
+            int ranNum = Random.Range(0,normalHeros.Length);
+            drawHeroGo = normalHeros[ranNum];
+        }else if (randomProb > 70.0f && randomProb <= 95.0f)
+        {
+            int ranNum = Random.Range(0, specialHeros.Length);
+            drawHeroGo = specialHeros[ranNum];
+        }
+        else if (randomProb > 95.0f && randomProb <= 99.0f)
+        {
+            int ranNum = Random.Range(0, epicHeros.Length);
+            drawHeroGo = epicHeros[ranNum];
+        }
+        else if (randomProb > 99.0f)
+        {
+            int ranNum = Random.Range(0, legendHeros.Length);
+            drawHeroGo = legendHeros[ranNum];
+        }
+        return drawHeroGo;
     }
 
 
